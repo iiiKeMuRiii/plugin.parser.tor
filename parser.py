8579b7760eedc257017c2a636e6d7465467704b0
iiiKeMuRiii <jbslimer@gmail.com>
2020-04-09 04:17:02 +0300

    Добавление ф-ий парсинг категорий

--- parser.py
+++ parser.py
@@ -50,88 +50,129 @@ addon_handle = int(sys.argv[1])
 #session = requests.session()
 #request = session.get(url, headers=headers)
 args = urllib.parse.parse_qs(sys.argv[2][1:])
-
 mode = args.get('mode', None)
 
 
 
 
 def main_CAT():
+    if DEBUG:
+        log('*+*+*+*+*+*+*+*+*+*+*+*+*+*+* main_cat()')
+    r = requests.get(url_Main, headers=headers)
+    r.encoding = 'utf-8'
+    xbmc.log('*************** Ссылка ' + str(r), xbmc.LOGDEBUG)
+    soup = BeautifulSoup(r.text, 'html.parser', parse_only=SoupStrainer('div', id='content'))
+    main_cat = soup.find_all('h2')[2:9]
 
-        r = requests.get(url_Main, headers=headers)
-        r.encoding = 'utf-8'
-        xbmc.log('*************** Ссылка ' + str(r), xbmc.LOGDEBUG)
-        soup = BeautifulSoup(r.text, 'html.parser', parse_only=SoupStrainer('div', id='content'))
-        main_cat = soup.find_all('h2')[2:9]
+    xbmc.log('*************** Ссылка ' + str(main_cat), xbmc.LOGDEBUG)
+
+    db_main_cat = []
+    for i in range(0, 7):
+        db_main_cat.append ({'title': main_cat[i].text})
+    xbmc.log('*************** СПИСОК ' + str(db_main_cat), xbmc.LOGDEBUG)
+    xbmc.log('*************** addon_handle ' + str(addon_handle), xbmc.LOGDEBUG)
+
+    for l in db_main_cat:
+        #listitem = xbmcgui.ListItem(i['title'])
+        #li = xbmcgui.ListItem (i['title'], iconImage='DefaultVideo.png')
+        #xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=li, isFolder=True)
+        add_main_CAT(l['title'], url_Main, 0, 'DefaultVideo.png')
+        #xbmcplugin.addDirectoryItem(handle=addon_handle, url=url_Main, listitem=li, isFolder=True)
+    xbmcplugin.endOfDirectory(addon_handle)
+
+def sub_CAT(indx_1):
+    if DEBUG:
+        log('*+*+*+*+*+*+*+*+*+*+*+*+*+*+* sub_CAT() ===== ' + str(indx_1))
+    r = requests.get(url_Main, headers=headers)
+    r.encoding = 'utf-8'
+    xbmc.log('*************** СВЯЗЬ ' + str(r), xbmc.LOGDEBUG)
+    soup = BeautifulSoup(r.text, 'html.parser', parse_only=SoupStrainer('div', id='content'))
+    sub_cat = soup.find_all('h3')[indx_1]('a')
+
+    if indx_1 == 2 or indx_1 == 3:
+        db_sub_cat = []
+        #l = re.split('\s', sub_cat[0:len(sub_cat)-48])
+        #l = list(filter (None, l))
+        #xbmc.log('*********//////////////// ЗНАЧЕНИЕ L ' + str(len(l)), xbmc.LOGDEBUG)
+        for i in sub_cat[0:len(sub_cat)-48]:
+            db_sub_cat.append ({'title': i.text})
+        for i in db_sub_cat:
+            add_main_CAT(i['title'], url_Main, 0, 'DefaultVideo.png')
+    xbmcplugin.endOfDirectory(addon_handle)
 
 
+def video_SER(indx_5):
+    #if DEBUG:
+    #    log('*+*+*+*+*+*+*+*+*+*+*+*+*+*+* sub_CAT() ===== ' + str(indx_1))
+    r = requests.get(url_Main, headers=headers)
+    r.encoding = 'utf-8'
+    xbmc.log('*************** СВЯЗЬ ' + str(r), xbmc.LOGDEBUG)
+    soup = BeautifulSoup(r.text, 'html.parser', parse_only=SoupStrainer('div', id='content'))
+    video_ser = soup.find_all('h3')[indx_5]('a')
+    xbmc.log('*************** СВЯЗЬ ' + str(video_ser), xbmc.LOGDEBUG)
 
-        db_cat = []
-        for i in range(0, 7):
-            db_cat.append ({'title': main_cat[i].text})
+    if indx_5 == 4 or indx_5 == 6 or indx_5 == 7:
+        db_ser = []
+        #xbmc.log('*********//////////////// ЗНАЧЕНИЕ L ' + str(len(l)), xbmc.LOGDEBUG)
+        for i in video_ser:
+            s = re.split('/', i.text.capitalize())
+            db_ser.append ({'title': s[0]})
+            xbmc.log('*********//////////////// ЗНАЧЕНИЕ СПИСКА ' + str(db_ser), xbmc.LOGDEBUG)
+        for i in db_ser:
+            add_main_CAT(i['title'], url_Main, 0, 'DefaultVideo.png')
+    elif indx_5 == 5:
+        db_ser = []
+        for i in video_ser[0:len(video_ser)-37]:
+            s = re.split('/', i.text)
+            db_ser.append ({'title': s[0]})
+            xbmc.log('*********//////////////// ЗНАЧЕНИЕ СПИСКА ' + str(db_ser), xbmc.LOGDEBUG)
+        for i in db_ser:
+            add_main_CAT(i['title'], url_Main, 0, 'DefaultVideo.png')
+    elif indx_5 == 8:
+        db_ser = []
+        for i in video_ser:
+            s = i.text.title()
+            db_ser.append ({'title': s})
+        for i in db_ser:
+            add_main_CAT(i['title'], url_Main, 0, 'DefaultVideo.png')
 
-
-        for l in db_cat:
-            #listitem = xbmcgui.ListItem(i['title'])
-            #li = xbmcgui.ListItem (i['title'], iconImage='DefaultVideo.png')
-            #xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=li, isFolder=True)
-            add_main_CAT(l['title'], url_Main, 0, 'DefaultVideo.png')
-            #xbmcplugin.addDirectoryItem(handle=addon_handle, url=url_Main, listitem=li, isFolder=True)
-        add_main_CAT('Next Page', url_Main, 1, 'DefaultVideo.png')
-        xbmcplugin.endOfDirectory(addon_handle)
-
-def sub_CAT():
-
-        r = requests.get(url_Main, headers=headers)
-        r.encoding = 'utf-8'
-        xbmc.log('*************** Ссылка ' + str(r), xbmc.LOGDEBUG)
-        soup = BeautifulSoup(r.text, 'html.parser', parse_only=SoupStrainer('div', id='content'))
-        sub_menu = soup.find_all('h3')[2].text
-
-        #xbmc.log('*************** Ссылка ' + str(main_cat), xbmc.LOGDEBUG)
-
-        sub_CAT = []
-        l = re.split('\s', sub_menu)[0:28]
-        l = list(filter (None, l))
-        for i in range(1, 27):
-            sub_CAT.append ({'title': l[i]})
-        #xbmc.log('*************** СПИСОК ' + str(sub_cat), xbmc.LOGDEBUG)
-        #xbmc.log('*************** addon_handle ' + str(addon_handle), xbmc.LOGDEBUG)
-
-        for l in sub_CAT:
-            #listitem = xbmcgui.ListItem(i['title'])
-            #li = xbmcgui.ListItem (i['title'], iconImage='DefaultVideo.png')
-            #xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=li, isFolder=True)
-            add_main_CAT(l['title'], url_Main, 0, 'DefaultVideo.png')
-            #xbmcplugin.addDirectoryItem(handle=addon_handle, url=url_Main, listitem=li, isFolder=True)
-        add_main_CAT('Next Page', url_Main, 1, 'DefaultVideo.png')
-        xbmcplugin.endOfDirectory(addon_handle)
-
+    xbmcplugin.endOfDirectory(addon_handle)
 
 def add_main_CAT(title, url, mode, iconimage):
-        u=sys.argv[0]+"?url="+urllib.parse.quote_plus(url)+"&mode="+str(mode)+"name="+urllib.parse.quote_plus(title)
-        ok = True
-        liz = xbmcgui.ListItem(title)
-        ok = xbmcplugin.addDirectoryItem(addon_handle, url=u, listitem=liz, isFolder=True) #*****************????????????????? правильный адрес ?????знак ? и =
-        #xbmcgui.Dialog().ok('Warning!', url)
-        xbmc.log('******* ::::::::::::: MODE ::::::::::: ******* ' + str(mode), xbmc.LOGDEBUG)
-        xbmc.log('******* ::::::::::::: addCatt ::::::::::: ******* ' + u, xbmc.LOGDEBUG)
-        return ok
+    u=sys.argv[0]+"?url="+urllib.parse.quote_plus(url)+"&mode="+str(mode)+"name="+urllib.parse.quote_plus(title)
+    ok = True
+    liz = xbmcgui.ListItem(title)
+    ok = xbmcplugin.addDirectoryItem(addon_handle, url=u, listitem=liz, isFolder=True) #*****************????????????????? правильный адрес ?????знак ? и =
+    #xbmcgui.Dialog().ok('Warning!', url)
+    xbmc.log('******* ::::::::::::: MODE ::::::::::: ******* ' + str(mode), xbmc.LOGDEBUG)
+    xbmc.log('******* ::::::::::::: addCatt ::::::::::: ******* ' + u, xbmc.LOGDEBUG)
+    return ok
 
 
 
-def log(self, description):
-        xbmc.log("[ADD-ON] '{} v{}': {}".format(_plugin, _version, description), xbmc.LOGNOTICE)
+def log(description):
+    xbmc.log("[ADD-ON] '{} v{}': {}".format(_plugin, _version, description), xbmc.LOGNOTICE)
 
 if mode == '1name=Next PageEE':
-        xbmc.log('******* ///////////////////// значение MODE ::::::::::: ******* ' + str(mode), xbmc.LOGDEBUG)
+    xbmc.log('******* ///////////////////// значение MODE ::::::::::: ******* ' + str(mode), xbmc.LOGDEBUG)
 
 
 if mode == None:
-        xbmcgui.Dialog().ok('Title: mode', 'NONE_NONE')
-elif str(mode[0]).split('=')[1] == 'Next Page':
-        xbmcgui.Dialog().ok('Title: mode', 'УРРРААА')
-        sub_CAT()
+    pass
+elif str(mode[0]).split('=')[1] == 'Зарубежные фильмы':
+    sub_CAT(2)
+elif str(mode[0]).split('=')[1] == 'Наши фильмы':
+    sub_CAT(3, 30, 29)
+elif str(mode[0]).split('=')[1] == 'Зарубежные сериалы':
+    video_SER(4)
+elif str(mode[0]).split('=')[1] == 'Наши сериалы':
+    video_SER(5)
+elif str(mode[0]).split('=')[1] == 'Телевизор':
+    video_SER(6)
+elif str(mode[0]).split('=')[1] == 'Мультипликация':
+    video_SER(7)
+elif str(mode[0]).split('=')[1] == 'Аниме':
+    video_SER(8)
 
 
 if __name__ == '__main__':
