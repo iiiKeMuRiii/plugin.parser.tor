a8f4717da5e3343002f7f99fbd0fa0a8d8a87a8e
iiiKeMuRiii <jbslimer@gmail.com>
2020-06-03 04:45:22 +0300

    Переход на коды категорий, вместо строковых значений. -> Нужно сделать сверку категории (жанра) фильма и последующую загрузку списка фильмов.

--- parser.py
+++ parser.py
@@ -53,6 +53,7 @@ args = urllib.parse.parse_qs(sys.argv[2][1:])
 tags = urllib.parse.parse_qs(sys.argv[3][1:])
 mode = args.get('mode', None)
 page = args.get('page', None)
+cats = args.get('cats', None)
 part_url = args.get('part_url', None)
 tag_part = args.get('tag_part', None)
 
@@ -71,23 +72,21 @@ def get_total_page(pages):
 
 def main_CAT():
     if DEBUG:
-        log('*+*+*+*+*+*+*+*+*+*+*+*+*+*+* main_cat() ')
-    xbmc.log('*************** Ссылка ' + str(mode), xbmc.LOGDEBUG)
+        log('*+*+*+*+*+*+*+*+*+*+*+*+*+*+* main_cat()')
     soup = BeautifulSoup(request(cat), 'html.parser', parse_only=SoupStrainer('div', id='content'))
     main_cat = soup.find_all('h2')[2:9]
     db_main_cat = []
     for i in range(0, 7):
         db_main_cat.append ({'title': main_cat[i].text,
-                             'index': i})
-    xbmc.log('*************** TEST ' + str(db_main_cat), xbmc.LOGDEBUG)
+                             'index': i+2})
     for l in db_main_cat:
-        add_CAT(l['title'], url_Main + cat, 0, 'tag', 'DefaultVideo.png')
+        add_CAT(l['title'], url_Main + cat, l['index'], 'tag', 'DefaultVideo.png')
     xbmcplugin.endOfDirectory(addon_handle)
-
+    #xbmcgui.Dialog().ok('Function MAIN!', str(mode))
 
 def sub_CAT(indx_1, tag_link):
     if DEBUG:
-        log('*+*+*+*+*+*+*+*+*+*+*+*+*+*+* sub_CAT() ===== ')
+        log('*+*+*+*+*+*+*+*+*+*+*+*+*+*+* sub_CAT() ===== ' + str(mode))
     soup = BeautifulSoup(request(cat), 'html.parser', parse_only=SoupStrainer('div', id='content'))
     sub_cat = soup.find_all('h3')[indx_1]('a') ## выборка категории - фильмы, сериалы, мультики...
     if indx_1 == 2 or indx_1 == 3 or indx_1 == 8:
@@ -120,7 +119,7 @@ def video_link():
     #tagg = tag
     page = 0
     total_pages = get_total_page(url_Main + '/search/' + str(page) + part_url[0] + str(mode[0]).split('=')[1] + '/')
-    xbmcgui.Dialog().ok('video_link MODE!', str(page))
+    xbmcgui.Dialog().ok('video_link MODE!', str(mode))
                                 #'search/0/1/010/0/'
 
     #if indx_1 == 2 or indx_1 == 3 or indx_1 == 8:
@@ -139,7 +138,7 @@ def video_link():
             db_mov_lnk.append ({'title': list_mov[1].text})
 
     for i in db_mov_lnk:
-        add_CAT(i['title'], url_Main + '/search/' + str(page) + part_url[0] + str(mode[0]).split('=')[1] + '/', 1, 'tag', 'DefaultVideo.png')
+        add_CAT(i['title'], url_Main + '/search/' + str(page) + part_url[0] + str(mode[0]).split('=')[1] + '/', 9, 'tag', 'DefaultVideo.png')
                                       # search/ + page/ + /1/010/0/
     #xbmcgui.Dialog().ok('Warning!', str(sys.argv[0]) + str(sys.argv[1]) + str(sys.argv[2]))
     #add_CAT('Next Page', url_Main.replace(url_Main, 'http://37.1.201.32/tag/1/Арт-хаус/2'), 1, 'DefaultVideo.png')
@@ -148,10 +147,10 @@ def video_link():
     #xbmcgui.Dialog().ok('video_link!', str(mode))
 
     #for i in range(1, int(total_pages)):
+    #if mode ==
+    xbmcgui.Dialog().ok('video_link MODE!', str(mode))
     z = soup.find('b').find('a')['href']
-    xbmcgui.Dialog().ok('video_link!', url_Main + z)
-    add_CAT('Next Page', 'http://222njbta.ihg.xrutor.org/search/1/1/010/0/Арт-хаус', 1, 'tag', 'DefaultVideo.png')
-    video_link()
+    add_CAT('Next Page', 'http://222njbta.ihg.xrutor.org/search/1/1/010/0/Арт-хаус', 10, 'tag', 'DefaultVideo.png')
     xbmcplugin.endOfDirectory(addon_handle)
 
 
@@ -196,17 +195,18 @@ def file_torrent():
     xbmcgui.Dialog().ok('video_link!', str(mode))
 
 def add_CAT(title, url, mode, part_url, iconimage):
-    u=sys.argv[0]+"?url="+urllib.parse.quote_plus(url)+"&part_url="+str(part_url)+"&mode="+str(mode)+"name="+urllib.parse.quote_plus(title)
+    u=sys.argv[0]+"?url="+urllib.parse.quote_plus(url)+"&part_url="+str(part_url)+"&mode="+str(mode)+"&name="+urllib.parse.quote_plus(title)
     ok = True
     liz = xbmcgui.ListItem(title)
     ok = xbmcplugin.addDirectoryItem(addon_handle, url=u, listitem=liz, isFolder=True) #*****************????????????????? правильный адрес ?????знак ? и =
 
     #liz.setArt ({ 'iconimage': 'http://c.radikal.ru/c20/2003/79/d0f12ddef3e1.jpg' })
 
-    #xbmcgui.Dialog().ok('Warning!', url)
+    #xbmcgui.Dialog().ok('Warning!', str(sys.argv[2].split('=')))
     #xbmc.log('******* ::::::::::::: MODE ::::::::::: ******* ' + str(mode), xbmc.LOGDEBUG)
-    xbmc.log('******* ::::::::::::: addCatt ::::::::::: ******* ' + u, xbmc.LOGDEBUG)
+    log('******* ::::::::::::: addCatt ::::::::::: ******* ' + urllib.parse.unquote(str(sys.argv[2].split('=')[2])))
     return ok
+
 
 ######## plugin://plugin.parser.tor/?url=http%3A%2F%2F37.1.201.32%2Fcategories%2F&mode=1name=%D0%AD%D1%80%D0%BE%D1%82%D0%B8%D0%BA%D0%B0
 def add_CAT2(title, url, mode, iconimage):
@@ -228,41 +228,48 @@ def log(description):
 if mode == '1name=Next PageEE':
     xbmc.log('******* ///////////////////// значение MODE ::::::::::: ******* ' + str(mode), xbmc.LOGDEBUG)
 
-
+xbmcgui.Dialog().ok('Function IF!', str(mode))
 if mode == None:
     pass
-elif str(mode[0]).split('=')[1] == 'Зарубежные фильмы':     # tag = search/0/1/010/0/:      search/  +  page/ +  1/010/0/
+elif mode[0] == '2':      # tag = search/0/1/010/0/:      search/  +  page/ +  1/010/0/
+    log('******* ///////////////////// West FILMS ::::::::::: *******')
     sub_CAT(2, '/1/010/0/')
-elif str(mode[0]).split('=')[1] == 'Наши фильмы':           # tag = search/ 0 /5/010/0/
+elif mode[0] == '3':           # tag = search/ 0 /5/010/0/
+    log('******* ///////////////////// USSR FILMS ::::::::::: *******')
     sub_CAT(3, '/5/010/0/')
-elif str(mode[0]).split('=')[1] == 'Зарубежные сериалы':    # tag = search/ 0/ 4/0/0/
+elif mode[0] == '4':    # tag = search/ 0/ 4/0/0/
+    log('******* ///////////////////// SERIAL WeST FILMS ::::::::::: *******')
     sub_CAT(4, '/4/0/0/')
-elif str(mode[0]).split('=')[1] == 'Наши сериалы':          # tag = search/ 0/ 16/0/0/
+elif mode[0] == '5':          # tag = search/ 0/ 16/0/0/
     tag_link = 'search/0/16/0/0/'
     sub_CAT(5, '/16/010/0/')
-elif str(mode[0]).split('=')[1] == 'Телевизор':             # tag = search/ 0/ 6/0/0/
+elif mode[0] == '6':             # tag = search/ 0/ 6/0/0/
     tag_link = 'search/0/6/0/0/'
     sub_CAT(6)
-elif str(mode[0]).split('=')[1] == 'Мультипликация':        # tag = tag/7/
+elif mode[0] == '7':        # tag = tag/7/
     tag_link = 'tag/7/'
     sub_CAT(7)
-elif str(mode[0]).split('=')[1] == 'Аниме':                 # tag = search/ 0/ 10/010/0/
+elif mode[0] == '8':                 # tag = search/ 0/ 10/010/0/
     sub_CAT(8, '/10/010/0/')
-elif int(mode[0].split('=')[0][-5]) == 2:
-    tag_part = 'tag/1/'
-    video_link()
-elif int(mode[0].split('=')[0][-5]) == 3:
-    video_link()
-elif int(mode[0].split('=')[0][-5]) == 4:
-    video_link()
-elif int(mode[0].split('=')[0][-5]) == 5:
-    video_link()
-elif int(mode[0].split('=')[0][-5]) == 6:
-    video_link()
-elif int(mode[0].split('=')[0][-5]) == 7:
-    video_link()
-elif int(mode[0].split('=')[0][-5]) == 8:
-    video_link()
+
+#elif urllib.parse.unquote(str(sys.argv[2].split('=')[2])):
+#    tag_part = 'tag/1/'
+#    video_link()
+#elif int(mode[0].split('=')[0][-5]) == 3:
+#    video_link()
+#elif int(mode[0].split('=')[0][-5]) == 4:
+#    video_link()
+#elif int(mode[0].split('=')[0][-5]) == 5:
+#    video_link()
+#elif int(mode[0].split('=')[0][-5]) == 6:
+#    video_link()
+#elif int(mode[0].split('=')[0][-5]) == 7:
+#    video_link()
+#elif int(mode[0].split('=')[0][-5]) == 8:
+#    video_link()
+
+
+# Нужно сделать сверку категории (жанра) фильма и последующую загрузку списка фильмов
 
 
 if __name__ == '__main__':
